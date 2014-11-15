72b11d53aff75f8b7b6714fe45a7fe6ca6a0c8e2
Bloodmanovski <bloodman@gshost.eu>
2014-11-15 19:42:06 +0100

    Working on cross binutils

---
+++ Externals/config.mk
@@ -0,0 +1,13 @@
+#
+# Externals config.mk File
+#
+
+-include ../../Makefile.cfg
+
+
+ifeq ($(ARCH),x86_64)
+	BFD := x86_64
+else
+	$(error No BFD translation for $(ARCH) in Externals/config.mk)
+endif
+
