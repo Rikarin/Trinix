fae0a0790fa7757fea3c9f9c459cbe2a25653006
Bloodmanovski <bloodman@gshost.eu>
2014-11-16 01:09:19 +0100

    Makefile...

--- Externals/config.mk
+++ Externals/config.mk
@@ -11,3 +11,9 @@ else
 	$(error No BFD translation for $(ARCH) in Externals/config.mk)
 endif
 
+
+OUTDIR	= $(TRXDIR)/Externals/Output/$(ARCHDIR)
+SYSROOT = $(TRXDIR)/Externals/Output/sysroot-$(BFD)
+
+HOST	= $(BFD)-unknown-trinix
+PATH	:= $(OUTDIR)-BUILD/bin:$(PATH)
