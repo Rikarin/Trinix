56771e5a1d5acbe8c3c2f75835ebb8deffa408d9
Bloodmanovski <bloodman@gshost.eu>
2014-11-16 18:32:54 +0100

    Successfuly cross-compiled binutils

--- Externals/config.mk
+++ Externals/config.mk
@@ -15,5 +15,7 @@ endif
 OUTDIR	= $(TRXDIR)/Externals/Output/$(ARCHDIR)
 SYSROOT = $(TRXDIR)/Externals/Output/sysroot-$(BFD)
 
-HOST	= $(BFD)-unknown-trinix
-PATH	:= $(OUTDIR)-BUILD/bin:$(PATH)
+HOST	:= $(BFD)-unknown-trinix
+#PATH	:= $(OUTDIR)-BUILD/bin:$(PATH)
+
+PARLEVEL = 4
