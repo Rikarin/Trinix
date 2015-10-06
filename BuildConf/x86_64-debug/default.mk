63ff0072a4414ed0bbd426ee3e031e44ab9d3ec5
Satoshi <bloodman@gshost.eu>
2015-10-07 00:09:25 +0200

    Makefiles

--- BuildConf/x86_64-debug/default.mk
+++ BuildConf/x86_64-debug/default.mk
@@ -2,12 +2,13 @@
 MODULES        += Storage/ATA
 #MODULES       += FileSystem/Ext2
 MODULES        += Input/PS2KeyboardMouse Input/Keyboard Input/Mouse
+#MODULES += Terminal/VTY
 
 # Shared Libs copied to System/Module dir and dynamically loaded while booting
 DYNMODS        += FileSystem/Ext2
 
 # Libraries .dylib .a .o
-USERLIBS       += 
+USERLIBS       += crt0.o core.dylib
 
 # Binaries
 USERBINS       +=
